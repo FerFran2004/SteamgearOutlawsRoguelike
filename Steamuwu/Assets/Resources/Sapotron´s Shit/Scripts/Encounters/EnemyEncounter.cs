e39a1a1ab6db381b9a7dd62e6bf7d578a188fb1a
FerFran2004 <ldv22c.ffranyutti@uartesdigitales.edu.mx>
2023-08-21 20:34:40 -0600

    Randomized Weighted Map Event Generation
    EVENT = LEVEL
    
    -The basic layout of the Map is completed
    
    -There are forced events that will always happen when lauching the map
    
    -The map has spawners that instantiate the events. Each event has a chance of spawning (the Enemy event has a 100% spawrate if the previous events didnt happen)

---
+++ Steamuwu/Assets/Resources/Sapotron´s Shit/Scripts/Encounters/EnemyEncounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEncounter : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Steamuwu/Assets/Resources/Sapotron´s Shit/Scripts/Spawners/RandomSpBhv.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RandomSpBhv : MonoBehaviour
+{
+    ////Prefabs to spawn 
+    //[SerializeField] private GameObject Enemy;
+    //[SerializeField] private GameObject Shop;
+    //[SerializeField] private GameObject Police;
+    //[SerializeField] private GameObject Casino;
+
+
+    //Percetage shit
+    [System.Serializable] 
+    public class EncounterManager
+    {
+        [SerializeField] public string Name;
+        [SerializeField] public GameObject Encounter;
+        [SerializeField] public int spawnRarity;
+
+    }
+
+    [SerializeField]
+    private List <EncounterManager> EncounterList = new List <EncounterManager>(); //Show encounter list in Editor
+    [SerializeField]
+    public int dropChance;
+
+    void Start()
+    {
+        // Spawn();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+           Spawn();
+           // CalculateEvents();
+        }
+        
+    }
+
+   public void CalculateEvents()
+    {
+
+        int calc_dropChance = Random.Range(0, 101);
+        if (calc_dropChance > dropChance )
+        {
+            Debug.Log("NO LOOT");
+            return;
+        }
+        if (calc_dropChance <= dropChance ) //IMPORTANT SHIT!!!!!
+        {
+            int encounterWeight = 0;
+            for (int i = 0; i < EncounterList.Count; i++)
+            {
+                encounterWeight += EncounterList[i].spawnRarity; //Sum of all rarities of encounters
+
+            }
+            Debug.Log("ENCOUNTER WEIGHT: " +  encounterWeight);
+
+            int randomValue = Random.Range (0, encounterWeight);
+
+            for (int j = 0;j < EncounterList.Count;j++)
+            {
+
+                if (randomValue <= EncounterList[j].spawnRarity)
+                {
+                    Instantiate(EncounterList[j].Encounter, transform.position, Quaternion.identity);
+                    return; //Stop the loop
+                }
+                randomValue -= EncounterList[j].spawnRarity;
+                Debug.Log("RANDOM VALUE DECREASED");
+            }
+        }
+    }    
+
+    public void Spawn()
+    {
+        ////1ST TRY---
+        //int SpawnDir = Random.Range(1,5);
+        //
+        //if (SpawnDir == 1)
+        //{
+        //    Instantiate(Enemy, gameObject.transform.position, Quaternion.identity);
+        //}
+        //if (SpawnDir == 2)
+        //{
+        //    Instantiate(Shop, gameObject.transform.position, Quaternion.identity);
+        //}
+        //if (SpawnDir == 3)
+        //{
+        //    Instantiate(Police, gameObject.transform.position, Quaternion.identity);
+        //}
+        //if (SpawnDir == 4)
+        //{
+        //    Instantiate(Casino, gameObject.transform.position, Quaternion.identity);
+        //}
+        //Debug.Log ("OBJECT INSTANTIATED: " +  SpawnDir);
+
+        //2ND TRY----
+
+        int chanceNumber = Random.Range(0, 101); //100 percentage
+        Debug.Log("CHANCE NUMBER: " + chanceNumber);
+
+        for (int i = 0; i < EncounterList.Count; i++)
+        {
+            if (chanceNumber <= EncounterList[i].spawnRarity)
+            {
+                Instantiate(EncounterList[i].Encounter, transform.position, Quaternion.identity);
+                Debug.Log("ITEM: " + EncounterList[i].Encounter + ", PROVIDED BY: " +  gameObject.name);
+                return;
+            }
+   
+           
+             
+        }
+
+    }
+
+
+
+}
